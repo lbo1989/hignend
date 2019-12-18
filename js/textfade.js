44a613df95a61d41f1f032803870a542c434ca15
unknown <s_b02@naver.com>
2019-12-18 15:29:13 +0900

    2019-12-18 modify subin

---
+++ js/textfade.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+   $('#imgs1').on('mouseenter',function(){
+
+       $('#text1').fadeOut(300).fadeIn(500);
+
+
+
+   });
+
+   $('#imgs2').on('mouseenter',function(){
+
+       $('#text2').fadeOut(300).fadeIn(500);
+
+
+
+   });
+
+    $('#imgs3').on('mouseenter',function(){
+
+       $('#text3').fadeOut(300).fadeIn(500);
+
+
+
+   });
+
+
+
+
+
+});
+
