5ae0b71c251fa8fe4e483fd5d6061f154b97d503
leeteakwan <xowk25@naver.com>
2019-12-18 16:52:39 +0900

    After ascss 수정

--- js/afterservice.js
+++ js/afterservice.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {
-  $('#accordion .subject').on('click', function(){
-    var content = $(this).siblings('.content');
-    if (content.css('display') == 'none') {
-      $('#accordion .content').slideUp();
-      content.slideDown();
-      } else {
-      content.slideUp();
-   }
-});
+$(document).ready(function () {
+    $('#accordion .subject').on('click', function () {
+        var content = $(this).siblings('.content');
+        if (content.css('display') == 'none') {
+            $('#accordion .content').slideUp();
+            content.slideDown();
+        } else {
+            content.slideUp();
+        }
+    });
+
+    $('.j_body>div').mouseover(function () {
+        $(this).children().children().css('opacity', '1');
+        $('.j').not(this).children().children().css('opacity', '0.4');
+    });
+        $('.j_body>div').mouseout(function () {
+        $('.j').children().children().css('opacity', '1');
+        });
 });
