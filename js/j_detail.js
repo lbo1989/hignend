8c4ff82d2afbcbc79b381c56ac44dac9d75b7af5
unknown <333jane333@naver.com>
2019-12-19 17:06:00 +0900

    fix bxSlider

--- js/j_detail.js
+++ js/j_detail.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $('.slider').bxSlider({
-        slideWidth: 500
-    });
+    let width = $('body').width();
+    console.log(width);
+    if (width < 768) {
+        $('.slider').bxSlider({
+            slideWidth: 300
+        });
+        $('#link').html("<br>");
+    } else {
+        $('.slider').bxSlider({
+            slideWidth: 500
+        });
+    }
 });
