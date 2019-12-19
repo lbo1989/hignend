06b035c1742844c258a12fb54a53219eb31c24a9
unknown <333jane333@naver.com>
2019-12-19 17:16:59 +0900

    j_detail.js fixed

--- js/j_detail.js
+++ js/j_detail.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     let width = $('body').width();
     console.log(width);
+    if(width <1024){
+        $('#link').html("<br>");
+    }
     if (width < 768) {
         $('.slider').bxSlider({
             slideWidth: 300
         });
-        $('#link').html("<br>");
     } else {
         $('.slider').bxSlider({
             slideWidth: 500
